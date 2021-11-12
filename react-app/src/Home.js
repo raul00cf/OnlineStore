e24c8df7c7965fdf0f0a323f15ff6cde714bedcc
Raul Cotrim <ferreiraraul99@gmail.com>
2021-11-12 11:15:14 -0300

    b

--- react-app/src/Home.js
+++ react-app/src/Home.js
@@ -15,6 +15,13 @@ import Masc4 from './images/masc4.jpg';
 import Masc5 from './images/masc5.jpg';
 import Masc6 from './images/masc6.jpg';
 
+import Filtro1 from './images/filtro1.jpg';
+import Filtro2 from './images/filtro2.jpg';
+import Filtro3 from './images/filtro3.jpg';
+import Filtro4 from './images/filtro4.jpg';
+import Filtro5 from './images/filtro5.jpg';
+import Filtro6 from './images/filtro6.png';
+
 const initialUser = {
   'nome': '',
   'sobrenome': '',
@@ -40,6 +47,7 @@ const adminUser = {
 }
 
 const initialProducts = [{
+  'tipo': 'mascara',
   'nome': 'Máscada preta tecido',
   'imagem': Masc1,
   'price': 23.50,
@@ -48,6 +56,7 @@ const initialProducts = [{
   'description': 'Máscara preta de tecido contra o Covid-19'
 },
 {
+  'tipo': 'mascara',
   'nome': 'Máscada preta pano',
   'imagem': Masc2,
   'price': 15.75,
@@ -56,6 +65,7 @@ const initialProducts = [{
   'description': 'Máscara confortável de pano contra o Covid-19'
 },
 {
+  'tipo': 'mascara',
   'nome': 'Máscada preta tecido quadriculada',
   'imagem': Masc3,
   'price': 29.99,
@@ -64,6 +74,7 @@ const initialProducts = [{
   'description': 'Máscara importada de pano quadriculado preta'
 },
 {
+  'tipo': 'mascara',
   'nome': 'Máscada cirúrgica branca',
   'imagem': Masc4,
   'price': 15.75,
@@ -72,6 +83,7 @@ const initialProducts = [{
   'description': 'Máscara nacional cirurgica branca'
 },
 {
+  'tipo': 'mascara',
   'nome': 'Máscada cirúrgica azul',
   'imagem': Masc5,
   'price': 15.75,
@@ -80,15 +92,69 @@ const initialProducts = [{
   'description': 'Máscara nacional cirurgica azul'
 },
 {
+  'tipo': 'mascara',
   'nome': 'Máscada 3M branca',
   'imagem': Masc6,
   'price': 59.99,
   'storage': 14,
   'sold': 1,
   'description': 'Máscara importada 3M branca contra Covid-19'
+},{
+  'tipo': 'filtro',
+  'nome': 'Máscara filtro Darth Vader',
+  'imagem': Filtro1,
+  'price': 79.99,
+  'storage': 10,
+  'sold': 0,
+  'description': 'Máscara filtro inspirada Darth Vader para Covid-19'
+},
+{
+  'tipo': 'filtro',
+  'nome': 'Máscada filtro branca',
+  'imagem': Filtro2,
+  'price': 65.50,
+  'storage': 15,
+  'sold': 5,
+  'description': 'Máscara filtro simples branca'
+},
+{
+  'tipo': 'filtro',
+  'nome': 'Máscara filtro alumínio',
+  'imagem': Filtro3,
+  'price': 59.99,
+  'storage': 8,
+  'sold': 2,
+  'description': 'Máscara filtro importada de alumínio'
+},
+{
+  'tipo': 'filtro',
+  'nome': 'Máscada filtro pano cinza',
+  'imagem': Filtro4,
+  'price': 64.99,
+  'storage': 40,
+  'sold': 10,
+  'description': 'Máscara filtro nacional de pano cinza contra o Covid-19'
+},
+{
+  'tipo': 'filtro',
+  'nome': 'Máscada filtro transparente',
+  'imagem': Filtro5,
+  'price': 29.99,
+  'storage': 66,
+  'sold': 14,
+  'description': 'Máscara filtro de plástico transparente'
+},
+{
+  'tipo': 'filtro',
+  'nome': 'Máscada filtro RGB Razer',
+  'imagem': Filtro6,
+  'price': 99.99,
+  'storage': 29,
+  'sold': 6,
+  'description': 'Máscara filtro importada com luzes RBG Razer'
 }];
 
-const initialProduct = 5;
+const initialProduct = 11;
 
 var users = [adminUser];
 
